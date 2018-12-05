53b0b878f66985ea6e01c86eae95f0a4fd1c492f
Sebastian <me@seb2k.de>
2018-12-05 11:13:05 +0100

    Set-AzureRmPolicyDefinitions

---
+++ Set-AzureRmPolicyDefinitions.ps1
@@ -0,0 +1,11 @@
+﻿$policyFolder = ".\"
+$managementGroup = Get-AzureRmManagementGroup | Out-GridView -PassThru
+$policyDescription = "Apply Diagnostics Settings"
+
+foreach($item in (gci $policyFolder)) {
+    $json = gc $item.FullName | ConvertFrom-Json
+    $mode = $json.mode | ConvertTo-Json
+    $policyRule = $json.policyRule | ConvertTo-Json -Depth 8 | % { [System.Text.RegularExpressions.Regex]::Unescape($_) }
+    $parameters = $json.parameters | ConvertTo-Json -Depth 8 | % { [System.Text.RegularExpressions.Regex]::Unescape($_) }
+    New-AzureRmPolicyDefinition -Name $item.BaseName -DisplayName $item.BaseName -Policy $policyRule -Description $policyDescription -Parameter $parameters -Mode $json.mode -ManagementGroupName $managementGroup.Name
+}
