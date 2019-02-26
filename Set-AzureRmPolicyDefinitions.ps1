bc1baa0090e46af2aa7d62cb0a675b4016f818aa
Sebastian <me@seb2k.de>
2019-02-26 20:57:54 +0100

    Update Set-AzureRmPolicyDefinitions.ps1
    late night commit :)

--- Set-AzureRmPolicyDefinitions.ps1
+++ Set-AzureRmPolicyDefinitions.ps1
@@ -9,3 +9,4 @@ foreach($item in (gci $policyFolder)) {
     $parameters = $json.parameters | ConvertTo-Json -Depth 8 | % { [System.Text.RegularExpressions.Regex]::Unescape($_) }
     New-AzureRmPolicyDefinition -Name $item.BaseName -DisplayName $item.BaseName -Policy $policyRule -Description $policyDescription -Parameter $parameters -Mode $json.mode -ManagementGroupName $managementGroup.Name
 }
+#
