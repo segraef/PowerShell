a6fdb4d6ce50f3a6cc67ef0bf2f2f1a9133df035
dwas01 <davidmcf@microsoft.com>
2019-08-14 01:56:38 +1200

    Updated Export-AzDOBuildReleaseDefinitions.ps1

--- Export-AzDOBuildReleaseDefinitions.ps1
+++ Export-AzDOBuildReleaseDefinitions.ps1
@@ -1,11 +1,22 @@
 <#
+	.NOTES
+		==============================================================================================
+		Copyright(c) Microsoft Corporation. All rights reserved.
+
+		File:		Export-AzDOBuildReleaseDefinitions.ps1
+
+		Purpose:	PowerShell - Exports -Azure DevOps Build Release Definitions
+
+		Version: 	1.0.0.0 - 1st August 2019 - Sebastian Gräf
+		==============================================================================================
+
     .SYNOPSIS
       Exports build and release definitions from a source Azure DevOps project and imports these into a destination project.
       If no destination project give it will only export build and release defintions and save it as JSON.
 
     .DESCRIPTION
       The script will create two folders with all exported build and release definitions as JSON.
-      Once a destination organization and project are given, the script will import the exported 
+      Once a destination organization and project are given, the script will import the exported
       build and release defintions into the target project.
 
       Required Modules (will be installed if not present): VSTeam
@@ -33,106 +44,263 @@
 
     .PARAMETER destinationSecureAccessToken
         Azure DevOps destination Secure Access Token.
-
-    .NOTES
-      Version:        1.0
-      Author:         Sebastian Gräf
-      Email:          segraef@microsoft.com
-      Creation Date:  08/13/2019
-      Purpose/Change: Initial script development
 #>
 
-Param (
-    [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$sourceAccount,
-    [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$sourceProject,
-    [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$sourcePersonalAccessToken,
-    [Parameter(Mandatory=$false,ValueFromPipeline=$true)][securestring]$sourceSecureAccessToken,
-    [Parameter(Mandatory=$False,ValueFromPipeline=$true)][string]$destinationAccount,
-    [Parameter(Mandatory=$False,ValueFromPipeline=$true)][string]$destinationProject,
-    [Parameter(Mandatory=$False,ValueFromPipeline=$true)][string]$destinationPersonalAccessToken,
-    [Parameter(Mandatory=$False,ValueFromPipeline=$true)][securestring]$destinationSecureAccessToken
+#Requires -Version 5
+
+[CmdletBinding()]
+param
+(
+	[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+	[string]$sourceAccount,
+	[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+	[string]$sourceProject,
+	[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+	[string]$sourcePersonalAccessToken,
+	[Parameter(Mandatory = $false, ValueFromPipeline = $true)]
+	[securestring]$sourceSecureAccessToken,
+	[Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+	[string]$destinationAccount,
+	[Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+	[string]$destinationProject,
+	[Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+	[string]$destinationPersonalAccessToken,
+	[Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+	[securestring]$destinationSecureAccessToken
 )
 
-Function Load-Module ($m) {
+#region - functions
+function Get-IsElevated
+{
+	#region - Get-IsElevated()
+	try
+	{
+		$WindowsId = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+		$WindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($WindowsId)
+		$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
 
-    if (Get-Module | Where-Object {$_.Name -eq $m}) {
-        Write-Output "Module $m is already imported."
-    }
-    else {
-        if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $m}) {
-            Import-Module $m -Verbose
-        }
-        else {
-            if (Find-Module -Name $m | Where-Object {$_.Name -eq $m}) {
-                Install-Module -Name $m -Force -Verbose -Scope CurrentUser
-                Import-Module $m -Verbose
-            }
-            else {
-                Write-Output "Module $m not imported, not available and not in online gallery, exiting."
-                EXIT 1
-            }
-        }
-    }
+		if ($WindowsPrincipal.IsInRole($adminRole))
+		{
+			return $true
+		}
+		else
+		{
+			return $false
+		}
+	}
+	catch [system.exception]
+	{
+		Write-Output "Error in Get-IsElevated() $($psitem.Exception.Message) Line:$($psitem.InvocationInfo.ScriptLineNumber) Char:$($psitem.InvocationInfo.OffsetInLine)"
+		exit
+	}
+	#endregion
 }
 
-Load-Module VSTeam
+function Import-ADOModule
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $false)]
+		[string]$module
+	)
 
-if($sourceAccount -and $sourceProject) {
-    # Set the source project
-    if($sourcePersonalAccessToken) {
-        Set-VSTeamAccount -Account $sourceAccount -PersonalAccessToken $sourcePersonalAccessToken
-    } elseif($sourcePersonalAccessToken) {
-        Set-VSTeamAccount -Account $sourceAccount -SecurePersonalAccessToken $sourceSecureAccessToken
-    } else {
-        Write-Output "Exiting script since no token given"
-    }
+	#region - Import-ADOModule()
+	try
+	{
+		if (Get-Module | Where-Object { $psitem.Name -eq $module })
+		{
+			Write-Output "Module $module is already imported."
+		}
+		else
+		{
+			if (Get-Module -ListAvailable | Where-Object { $psitem.Name -eq $module })
+			{
+				Import-Module $module -Verbose
+			}
+			else
+			{
+				if (Find-Module -Name $module | Where-Object { $psitem.Name -eq $module })
+				{
+					$paramInstallModule = @{
+						Name    = $module
+						Force   = $true
+						Verbose = $true
+						Scope   = 'CurrentUser'
+					}
+					Install-Module @paramInstallModule
 
-    # Get all release definitions
-    $releaseDefinitions = Get-VSTeamReleaseDefinition -ProjectName $sourceProject
+					$paramImportModule = @{
+						Name    = $module
+						Verbose = $true
+					}
+					Import-Module @paramImportModule
+				}
+				else
+				{
+					Write-Output "Module $module not imported, not available and not in online gallery, exiting."
+					exit
+				}
+			}
+		}
+	}
+	catch [system.exception]
+	{
+		Write-Output "Error in Load-Module() $($psitem.Exception.Message) Line:$($psitem.InvocationInfo.ScriptLineNumber) Char:$($psitem.InvocationInfo.OffsetInLine)"
+		exit
+	}
+	#endregion
+}
+#endregion
 
-    # Get all build definitions
-    $buildDefinitions = Get-VSTeamBuildDefinition -ProjectName $sourceProject
+#region - Export ADO Build Release Definitions
+Clear-Host
+Write-Output "Script Started"
 
-    # Create definition folders
-    $buildDefinitionDirectory = New-Item "$sourceAccount.$sourceProject.BuildDefinitions" -ItemType Directory -Force
-    $releaseDefinitionDirectory = New-Item "$sourceAccount.$sourceProject.ReleaseDefinitions" -ItemType Directory -Force
+if (Get-IsElevated)
+{
+	try
+	{
+		(Get-Host).UI.RawUI.WindowTitle = "$env:USERDOMAIN\$env:USERNAME (Elevated)"
+		$timeSync = Get-Date
+		$timeSync = $timeSync.ToString()
 
-    # Export build defintions
-    foreach($buildDefinition in $buildDefinitions) {
-        $fileName = $buildDefinitionDirectory.FullName + "\" + $buildDefinition.Name + ".json"
-        Get-VSTeamBuildDefinition -ProjectName $sourceProject -Id $buildDefinition.ID -json | Out-File $fileName
-    }
-    Write-Output "Your build definitions can be found here: $buildDefinitionDirectory"
+		Write-Output "Script is running in an elevated PowerShell host."
+		Write-Output "Start time: $timeSync`n"
 
-    # Export release defintions
-    foreach($releaseDefinition in $releaseDefinitions) {
-        $fileName = $releaseDefinitionDirectory.FullName + "\" + $releaseDefinition.Name + ".json"
-        Get-VSTeamReleaseDefinition -ProjectName $sourceProject -Id $releaseDefinition.ID -json | Out-File $fileName
-    }
-    Write-Output "Your release definitions can be found here: $releaseDefinitionDirectory"
+		#region - Import-ADOModule()
+		$paramLoadModule = @{
+			module = 'VSTeam'
+		}
+		Import-ADOModule @paramLoadModule
+		#endregion
+
+		#region - Set Source Project
+		if ($sourceAccount -and $sourceProject)
+		{
+			if ($sourcePersonalAccessToken)
+			{
+				$paramSetVSTeamAccount = @{
+					Account			    = $sourceAccount
+					PersonalAccessToken = $sourcePersonalAccessToken
+				}
+				Set-VSTeamAccount @paramSetVSTeamAccount
+			}
+			elseif ($sourcePersonalAccessToken)
+			{
+				$paramSetVSTeamAccount = @{
+					Account				      = $sourceAccount
+					SecurePersonalAccessToken = $sourceSecureAccessToken
+				}
+				Set-VSTeamAccount @paramSetVSTeamAccount
+			}
+			else
+			{
+				Write-Output "Exiting script since no token given"
+				exit
+			}
+
+			# Get all release definitions
+			$paramGetVSTeamReleaseDefinition = @{
+				ProjectName = $sourceProject
+			}
+			$releaseDefinitions = Get-VSTeamReleaseDefinition @paramGetVSTeamReleaseDefinition
+
+			# Get all build definitions
+			$paramGetVSTeamBuildDefinition = @{
+				ProjectName = $sourceProject
+			}
+			$buildDefinitions = Get-VSTeamBuildDefinition @paramGetVSTeamBuildDefinition
+
+			# Create definition folders
+			$buildDefinitionDirectory = New-Item "$sourceAccount.$sourceProject.BuildDefinitions" -ItemType Directory -Force
+			$releaseDefinitionDirectory = New-Item "$sourceAccount.$sourceProject.ReleaseDefinitions" -ItemType Directory -Force
+
+			# Export build defintions
+			foreach ($buildDefinition in $buildDefinitions)
+			{
+				$fileName = $buildDefinitionDirectory.FullName + "\" + $buildDefinition.Name + ".json"
+				$paramGetVSTeamBuildDefinition = @{
+					ProjectName = $sourceProject
+					Id		    = $buildDefinition.ID
+					json	    = $true
+				}
+				Get-VSTeamBuildDefinition @paramGetVSTeamBuildDefinition | Out-File $fileName
+			}
+			Write-Output "Your build definitions can be found here: $buildDefinitionDirectory"
+
+			# Export release defintions
+			foreach ($releaseDefinition in $releaseDefinitions)
+			{
+				$fileName = $releaseDefinitionDirectory.FullName + "\" + $releaseDefinition.Name + ".json"
+				$paramGetVSTeamReleaseDefinition = @{
+					ProjectName = $sourceProject
+					Id		    = $releaseDefinition.ID
+					json	    = $true
+				}
+				Get-VSTeamReleaseDefinition @paramGetVSTeamReleaseDefinition | Out-File $fileName
+			}
+			Write-Output "Your release definitions can be found here: $releaseDefinitionDirectory"
+		}
+		#endregion
+
+		#region - Set Destination Project
+		if ($destinationAccount -and $destinationProject)
+		{
+			if ($destinationPersonalAccessToken)
+			{
+				Set-VSTeamAccount -Account $destinationAccount -PersonalAccessToken $destinationPersonalAccessToken
+			}
+			elseif ($destinationSecureAccessToken)
+			{
+				Set-VSTeamAccount -Account $sourceAccount -SecurePersonalAccessToken $destinationSecureAccessToken
+			}
+			else
+			{
+				Write-Output "Exiting script since no token given"
+			}
+
+			# Import release defintions
+			$releaseDefinitions = Get-ChildItem $releaseDefinitionDirectory.FullName
+			foreach ($releaseDefinition in $releaseDefinitions)
+			{
+				$fileName = $releaseDefinition.FullName
+				$paramAddVSTeamReleaseDefinition = @{
+					ProjectName = $destinationProject
+					inFile	    = $fileName
+				}
+				Add-VSTeamReleaseDefinition @paramAddVSTeamReleaseDefinition
+			}
+
+			# Import build defintions
+			$buildDefinitions = Get-ChildItem $buildDefinitionDirectory.FullName
+			foreach ($buildDefinition in $buildDefinitions)
+			{
+				$fileName = $buildDefinition.FullName
+				$paramAddVSTeamReleaseDefinition = @{
+					ProjectName = $destinationProject
+					inFile	    = $fileName
+				}
+				Add-VSTeamReleaseDefinition @paramAddVSTeamReleaseDefinition
+			}
+		}
+		#endregion
+
+		$timeSync = Get-Date
+		$timeSync = $timeSync.ToString()
+		Write-Output "`nEnded at: $timeSync"
+	}
+	catch [system.exception]
+	{
+		Write-Output "Error:$($psitem.Exception.Message) Line:$($psitem.InvocationInfo.ScriptLineNumber) Char:$($psitem.InvocationInfo.OffsetInLine)"
+		exit
+	}
+}
+else
+{
+	Write-Output "Please start the script from an elevated PowerShell host."
+	exit
 }
 
-if($destinationAccount -and $destinationProject) {
-    # Set the destination project
-    if($destinationPersonalAccessToken) {
-        Set-VSTeamAccount -Account $destinationAccount -PersonalAccessToken $destinationPersonalAccessToken
-    } elseif($destinationSecureAccessToken) {
-        Set-VSTeamAccount -Account $sourceAccount -SecurePersonalAccessToken $destinationSecureAccessToken
-    } else {
-        Write-Output "Exiting script since no token given"
-    }
-
-    # Import release defintions
-    $releaseDefinitions = Get-ChildItem $releaseDefinitionDirectory.FullName
-    foreach($releaseDefinition in $releaseDefinitions) {
-        $fileName = $releaseDefinition.FullName
-        Add-VSTeamReleaseDefinition -ProjectName $destinationProject -inFile $fileName
-    }
-
-    # Import build defintions
-    $buildDefinitions = Get-ChildItem $buildDefinitionDirectory.FullName
-    foreach($buildDefinition in $buildDefinitions) {
-        $fileName = $buildDefinition.FullName
-        Add-VSTeamReleaseDefinition -ProjectName $destinationProject -inFile $fileName
-    }
-}
+Write-Output "Script Completed."
+#endregion
