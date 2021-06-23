7cc3ef8930be3843012904ba61b1cb22af9e0cf6
Sebastian Gräf <segraef@microsoft.com>
2021-06-23 13:37:20 +0800

    fixes

--- Scripts/Set-AzureRmPolicyDefinitions.ps1
+++ Scripts/Set-AzureRmPolicyDefinitions.ps1
@@ -5,7 +5,7 @@ $policyDescription = "Apply Diagnostics Settings"
 foreach ($item in (Get-Childitem $policyFolder)) {
     $json = Get-Content $item.FullName | ConvertFrom-Json
     $mode = $json.mode | ConvertTo-Json
-    $policyRule = $json.policyRule | ConvertTo-Json -Depth 8 | % { [System.Text.RegularExpressions.Regex]::Unescape($_) }
-    $parameters = $json.parameters | ConvertTo-Json -Depth 8 | % { [System.Text.RegularExpressions.Regex]::Unescape($_) }
+    $policyRule = $json.policyRule | ConvertTo-Json -Depth 8 | ForEach-Object { [System.Text.RegularExpressions.Regex]::Unescape($_) }
+    $parameters = $json.parameters | ConvertTo-Json -Depth 8 | ForEach-Object { [System.Text.RegularExpressions.Regex]::Unescape($_) }
     New-AzureRmPolicyDefinition -Name $item.BaseName -DisplayName $item.BaseName -Policy $policyRule -Description $policyDescription -Parameter $parameters -Mode $json.mode -ManagementGroupName $managementGroup.Name
 }
